ab8ea4c62ccd2d6e33134348b6f7cc4e2b00b421
Julius Taylor <juliustaylor@protonmail.com>
2021-06-07 17:24:49 +0200

    Fix the layout provisionally. The bottom border of the search bar is still off..

--- components/layout.js
+++ components/layout.js
@@ -8,13 +8,29 @@ export const siteTitle = "ML Gloss";
 
 export default function Layout({ children, catsAndNames, toc }) {
   return (
-    <div className="min-h-full md:container md:mx-auto px-2 md:px-2 dark:bg-gray-700">
+    <div className="min-h-full px-2 md:container md:mx-auto md:px-2 dark:bg-gray-700">
       <Head>
         <link rel="icon" href="/favicon.ico" />
         <meta name="ML Implementations" content="Baseline ML models" />
         <meta name="og:title" content={siteTitle} />
         <title>{siteTitle}</title>
       </Head>
+      <div className="sticky top-0 z-50 flex flex-row bg-white dark:bg-gray-700 lg:h-16">
+        <div className="inline-flex items-center mt-2 text-3xl dark:bg-gray-700 lg:w-64">
+          <Link href="/">
+            <a className="align-middle dark:text-indigo-50">ML Gloss</a>
+          </Link>
+        </div>
+        <div className="flex h-full w-full justify-between lg:px-24">
+          <input
+            className="w-full border-b dark:bg-gray-700 dark:text-indigo-200 dark:border-indigo-400 "
+            defaultValue={`Quick search for anything...`}
+          />
+          <div className="inline-flex items-center border-b dark:border-indigo-400">
+            <a className="dark:text-indigo-50">GitHub</a>
+          </div>
+        </div>
+      </div>
       <div className="justify-between min-h-full md:flex">
         <SideBar
           catsAndNames={catsAndNames}
@@ -26,30 +42,14 @@ export default function Layout({ children, catsAndNames, toc }) {
           </div>
           <div className="min-h-full">
             <Outline
-              className="sticky top-0 flex flex-col pt-10 lg:w-60"
+              className="sticky flex flex-col pt-10 top-16 lg:w-60"
               toc={toc}
             />
           </div>
         </div>
       </div>
       {/*
-      <div className="sticky top-0 z-50 flex flex-row w-full mx-auto bg-white dark:bg-gray-700 lg:h-16">
-        <div className="inline-flex items-center mt-2 text-3xl dark:bg-gray-700 lg:w-auto lg:pr-10">
-          <Link href="/">
-            <a className="align-middle dark:text-indigo-50">ML Gloss</a>
-          </Link>
-        </div>
-        <div className="container flex flex-1 mt-2 lg:pl-44">
-          <input
-            className="w-full h-full border-b dark:bg-gray-700 dark:text-indigo-200 dark:border-indigo-400 "
-            defaultValue={`Quick search for anything...`}
-          />
-          <div className="inline-flex items-center border-b dark:border-indigo-400">
-            <a className="dark:text-indigo-50">GitHub</a>
-          </div>
-        </div>
-      </div>
-      */}
+       */}
     </div>
   );
 }
