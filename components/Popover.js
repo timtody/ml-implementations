23b382f7097faa41bf55f2e722ab81f039b4032b
Julius Taylor <juliustaylor@protonmail.com>
2021-07-23 14:40:06 +0200

    Change placement of container

--- components/Popover.js
+++ components/Popover.js
@@ -7,7 +7,7 @@ export default function MyPopover({ buttonText, content }) {
   let [referenceElement, setReferenceElement] = useState();
   let [popperElement, setPopperElement] = useState();
   let { styles, attributes } = usePopper(referenceElement, popperElement, {
-    placement: "top",
+    placement: "bottom-start",
   });
 
   return (
@@ -20,9 +20,12 @@ export default function MyPopover({ buttonText, content }) {
         ref={setPopperElement}
         style={styles.popper}
         {...attributes.popper}
-        className="grid grid-cols-3 gap-2 bg-gray-50 rounded p-2 z-30 shadow-sm"
+        className="z-50"
       >
-        {content}
+        <div className="bg-gray-50 rounded p-2 shadow-sm">
+          <p>All tags:</p>
+          <div className="grid grid-cols-3 gap-2">{content}</div>
+        </div>
       </Popover.Panel>
     </Popover>
   );
